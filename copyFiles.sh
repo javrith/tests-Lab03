f3587575cf038ce4015868c16ab30bc2449d77b1
chalonverse <sanjay12@gmail.com>
2025-07-26 16:49:01 -0700

    Cleanup

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 cp ../Lab03/CollisionComponent.cpp .
+cp ../Lab03/Component.cpp .
+cp ../Lab03/Transform.cpp .
+cp ../Lab03/Actor.cpp .
+cp ../Lab03/CollisionComponent.h .
+cp ../Lab03/Component.h .
+cp ../Lab03/Transform.h .
+cp ../Lab03/Actor.h .
